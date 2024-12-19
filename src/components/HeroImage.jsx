d2473fab4d56a89f939e15620cb0e08a58414cb9
Neil Taggart <neil.taggart@adaptive-online.com>
2024-12-18 23:48:43 -0400

    Unsuccessful attempt to get hero image with cloud frame

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -12,28 +12,56 @@ const IMAGE_PROPS = {
   size: 300,
 };
 
+// Simplified cloud path - scaled up by 5.76x (was 57.6x)
+const CLOUD_PATH = "M198 180c16.434 0 28.8-12.366 28.8-28.8s-12.366-32.4-28.8-32.4c0-41.093-34.507-72-75.6-72-36.37 0-69.12 23.122-75.6 57.6h-7.2C17.902 104.4 3.6 123.454 3.6 144s15.454 36 36 36z";
+
 export default function HeroImage() {
   return (
     <motion.div
       {...ANIMATION_CONFIG}
-      className={`relative w-[${IMAGE_PROPS.size}px] h-[${IMAGE_PROPS.size}px]`}
+      className="relative w-[400px] h-[400px] -mt-8"
     >
-      {/* Black background layer */}
-      <div className="absolute inset-0 bg-black rounded-full" />
-
-      {/* Green frame layer */}
-      <div className="absolute inset-0">
-        <div className="w-full h-full bg-primary rounded-full border-4 border-primary" />
-      </div>
-      
-      {/* Image layer */}
-      <div className="absolute inset-[4px] overflow-hidden rounded-full">
-        <img
-          src={IMAGE_PROPS.src}
-          alt={IMAGE_PROPS.alt}
-          className="w-full h-full object-cover"
+      <svg 
+        viewBox="0 0 3000 2500" 
+        className="w-full h-full"
+      >
+        {/* ViewBox outline */}
+        <rect
+          x="0"
+          y="0"
+          width="3000"
+          height="2500"
+          fill="none"
+          stroke="blue"
+          strokeWidth="1"
         />
-      </div>
+
+        <defs>
+          <clipPath id="cloudClip">
+            <path d={CLOUD_PATH} transform="translate(800, 200) scale(8)" />
+          </clipPath>
+        </defs>
+
+        {/* Clipped image */}
+        <image
+          href={IMAGE_PROPS.src}
+          width="1800"
+          height="1800"
+          x="350"
+          y="350"
+          preserveAspectRatio="xMidYMid slice"
+          clipPath="url(#cloudClip)"
+        />
+
+        {/* Single outline */}
+        <path
+          d={CLOUD_PATH}
+          transform="translate(800, 200) scale(8)"
+          fill="none"
+          className="stroke-primary"
+          strokeWidth="3"
+        />
+      </svg>
     </motion.div>
   );
 }
